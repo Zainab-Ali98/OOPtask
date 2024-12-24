9af1d8efd69df0934d8255b52c25c92468ebce67
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-24 14:24:59 +0300

    Add project files.

---
+++ OOPtask/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask.Models
+{
+    public class Student
+    {
+        public string GetInfo( string name, string major, double GPA)
+        {
+
+            return " name :" + name +"  " + " major " + major + " " + "GPA "+ GPA;
+         
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ OOPtask/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Reflection;
+using OOPtask.Models;
+
+Student emp = new Student();
+
+string name, major;
+
+Console.WriteLine("enter name");
+name = Console.ReadLine();
+
+Console.WriteLine("enter your major");
+major= Console.ReadLine();
+
+Console.WriteLine("enter your GPA");
+double GPA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(emp.GetInfo(name, major, GPA));
